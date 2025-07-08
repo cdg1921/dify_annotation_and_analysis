1aa24b8875354387cb311b198c6fd6103957d9f4
cdg1921 <cdg1921@qq.com>
2025-07-08 22:37:20 +0800

    添加dateset、document、message、tenant事件蓝图信号注释

--- api/events/message_event.py
+++ api/events/message_event.py
@@ -1,4 +1,6 @@
 from blinker import signal
 
+# cdg: 信号的名称是message-was-created，信号的接收器是message_was_created。
+# cdg: 当message被创建时，message_was_created信号被触发，所有连接的接收器都会收到这个信号。
 # sender: message, kwargs: conversation
 message_was_created = signal("message-was-created")
